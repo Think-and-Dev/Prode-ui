d2ae2a7ab47452f4ff98d654413751ba788d8ce5
Lopez Gonzalo <lopezgonzalo.jsx@gmail.com>
2022-09-16 15:38:13 -0300

    chore: removed PageChange

--- pages/_app.js
+++ pages/_app.js
@@ -21,19 +21,9 @@ import App from "next/app";
 import Head from "next/head";
 import Router from "next/router";
 
-import PageChange from "/components/PageChange/PageChange.js";
-
 import "/styles/scss/nextjs-material-kit.scss?v=1.2.0";
 import Web3ContextProvider from "../contexts/Web3Context";
 
-Router.events.on("routeChangeStart", (url) => {
-  console.log(`Loading: ${url}`);
-  document.body.classList.add("body-page-transition");
-  ReactDOM.render(
-    <PageChange path={url} />,
-    document.getElementById("page-transition")
-  );
-});
 Router.events.on("routeChangeComplete", () => {
   ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
   document.body.classList.remove("body-page-transition");
